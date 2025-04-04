6b84fb7c4e08c5af3e5c4a00e5caf45d0d6fe9fa
Ashika Gupta <ashikagupta1503@gmail.com>
2025-04-04 11:00:34 +0000

    Sync LeetCode submission Runtime - 16 ms (18.77%), Memory - 49.3 MB (90.70%)

--- Leetcode_By_Ashika/0169-majority-element/solution.java
+++ Leetcode_By_Ashika/0169-majority-element/solution.java
@@ -1,24 +1,41 @@
-class Solution {
-    public int majorityElement(int[] nums) {
-        Arrays.sort(nums); 
-        int n=nums.length; 
-        int ans=nums[0];
-         int freq=1;
-         for (int i = 1; i < n; i++) {
-            if (nums[i] == nums[i - 1]) {
-                freq++; // Increase frequency
-            } else {
-                freq = 1;
-                 ans = nums[i]; // Reset frequency for new number
-            }
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         Arrays.sort(nums); 
+//         int n=nums.length; 
+//         int ans=nums[0];
+//          int freq=1;
+//          for (int i = 1; i < n; i++) {
+//             if (nums[i] == nums[i - 1]) {
+//                 freq++; 
+//             } else {
+//                 freq = 1;
+//                  ans = nums[i]; 
+//             }
             
-            if (freq > n/2) { 
-                return ans;
+//             if (freq > n/2) { 
+//                 return ans;
                
-            }
-        }
+//             }
+//         }
         
-        // Since the majority element always exists, we return ans directly.
-        return ans;
+       
+//         return ans;
+//     }
+// }
+
+class Solution{
+    public int majorityElement(int[] nums){
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            int count=map.getOrDefault(nums[i], 0) + 1;
+            map.put(nums[i],count);
+
+            if(map.get(nums[i])>n/2){
+                return nums[i];
+            }
+        }return -1;
     }
 }
+
+
