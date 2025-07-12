e47419f2111d765150d9361d470c9b3f0a3bfc49
Ashika Gupta <ashikagupta1503@gmail.com>
2025-07-12 09:15:22 +0000

    Sync LeetCode submission Runtime - 1 ms (99.82%), Memory - 62 MB (19.47%)

--- Leetcode_By_Ashika/0121-best-time-to-buy-and-sell-stock/solution.java
+++ Leetcode_By_Ashika/0121-best-time-to-buy-and-sell-stock/solution.java
@@ -2,17 +2,18 @@ class Solution {
     public int maxProfit(int[] prices) {
         int min=prices[0]; int maxProfit=0;
         for(int i=1;i<prices.length;i++){
-            if(prices[i]<min){
-                min=prices[i];
+            if(prices[i]>min){
+                 int profit=prices[i]-min;
+                  maxProfit=Math.max(profit,maxProfit);
+               
             }else{
-               int profit=prices[i]-min;
-               if(maxProfit<profit){
-                   maxProfit=profit;
+                  min=prices[i];
+              
                }
             }
                 
             
-        }
-    return maxProfit;  
+        
+    return maxProfit;
    }
 }
