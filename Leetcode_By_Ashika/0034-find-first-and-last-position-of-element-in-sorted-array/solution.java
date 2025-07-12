36b291b2cdd6d0305ef3d5de3613b70c0124d51b
Ashika Gupta <ashikagupta1503@gmail.com>
2025-07-12 08:14:41 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 46.5 MB (9.08%)

--- Leetcode_By_Ashika/0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
+++ Leetcode_By_Ashika/0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
@@ -1,18 +1,46 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int first = -1, last = -1;
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+        return new int[] {first, last};
+    }
 
-        // Finding the first and last occurrence using a single pass
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                if (first == -1) {
-                    first = i; // First occurrence
-                }
-                last = i; // Update last occurrence
+    private int findFirst(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int index = -1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                index = mid;       // store index
+                right = mid - 1;   // go left to find earlier index
+            } else if (nums[mid] < target) {
+                left = mid + 1;    // go right
+            } else {
+                right = mid - 1;   // go left
             }
         }
+        return index;
+    }
 
-        return new int[]{first, last};
+    private int findLast(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int index = -1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                index = mid;       // store index
+                left = mid + 1;    // go right to find later index
+            } else if (nums[mid] < target) {
+                left = mid + 1;    // go right
+            } else {
+                right = mid - 1;   // go left
+            }
+        }
+        return index;
     }
 }
 
