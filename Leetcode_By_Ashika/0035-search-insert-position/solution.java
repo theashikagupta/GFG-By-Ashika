66ab2f6295fb0270e10c6d1540aa33c01d150abb
Ashika Gupta <ashikagupta1503@gmail.com>
2025-07-12 07:36:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.3 MB (28.48%)

--- Leetcode_By_Ashika/0035-search-insert-position/solution.java
+++ Leetcode_By_Ashika/0035-search-insert-position/solution.java
@@ -1,20 +1,21 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        
+        int left = 0;
+        int right = nums.length - 1;
+
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            
+
             if (nums[mid] == target) {
-                return mid; // Target found
+                return mid;
             } else if (nums[mid] < target) {
-                left = mid + 1; // Move right
+                left = mid + 1;
             } else {
-                right = mid - 1; // Move left
+                right = mid - 1;
             }
         }
-        
-        return left; // The correct insertion position
+
+        return left;
     }
 }
 
