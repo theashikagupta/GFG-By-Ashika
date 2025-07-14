5b0b9dac5c26c4ba2c02aac68cf0411bd6faca91
Ashika Gupta <ashikagupta1503@gmail.com>
2025-07-14 07:29:37 +0000

    Sync LeetCode submission Runtime - 6 ms (48.55%), Memory - 46.1 MB (40.29%)

---
+++ Leetcode_By_Ashika/0240-search-a-2d-matrix-ii/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m=matrix.length; int n=matrix[0].length;
+        int r=0; int c=n-1;
+        while(r<m && c>=0){
+            if(target==matrix[r][c]){
+                return true;
+            }else if(target>matrix[r][c]){
+                r++;
+            }else{
+                c--;
+            }
+        }
+        return false;
+    }
+}
