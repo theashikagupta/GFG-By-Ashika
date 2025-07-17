002c193c05f03cfc3227b7ca6241299d3751676c
Ashika Gupta <ashikagupta1503@gmail.com>
2025-07-17 10:01:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.4 MB (50.00%)

--- Leetcode_By_Ashika/1013-fibonacci-number/solution.java
+++ Leetcode_By_Ashika/1013-fibonacci-number/solution.java
@@ -1,22 +1,14 @@
 class Solution {
     public int fib(int n) {
+        if (n == 0) return 0;
+        int dp[]=new int[n+1];
         
-        if(n==0){
-            return 0;
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        else{
-            int a=0;
-            int b=1;
-            int count=2;
-            while(count<=n){
-                int c=a+b;
-                a=b;
-                b=c;
-                count++;
-
-                
-               
-            }return b;
-        }
+      
+     return dp[n]; 
     }
 }
