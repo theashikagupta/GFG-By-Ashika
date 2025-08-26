08f633cfe5b20fb3465c5ebacfedd6f357dd311d
Ashika Gupta <ashikagupta1503@gmail.com>
2025-08-26 03:13:28 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 45.5 MB (43.63%)

---
+++ Leetcode_By_Ashika/0268-missing-number/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int m=0;
+        int n=nums.length;
+        for(int i=0;i<=n;i++){
+            m^=i;
+        }
+        for(int i=0;i<n;i++){
+            m^=nums[i];
+        }
+        return m;
+    }
+}
