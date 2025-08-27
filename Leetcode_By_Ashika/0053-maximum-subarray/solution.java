8db2066a16724ec001c59d4d5df777e1e7122298
Ashika Gupta <ashikagupta1503@gmail.com>
2025-08-27 05:39:55 +0000

    Sync LeetCode submission Runtime - 1 ms (99.49%), Memory - 57.4 MB (12.82%)

--- Leetcode_By_Ashika/0053-maximum-subarray/solution.java
+++ Leetcode_By_Ashika/0053-maximum-subarray/solution.java
@@ -1,14 +1,26 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int cs = nums[0];  // current sum
-        int ms = nums[0];  // max sum so far
-
-        for (int i = 1; i < nums.length; i++) {
-            cs = Math.max(nums[i], cs + nums[i]);
-            ms = Math.max(ms, cs);
+        int cs=nums[0];
+        int ms=nums[0];
+        int n=nums.length;
+        for(int i=1;i<n;i++){
+            cs=Math.max(nums[i],cs+nums[i]);
+            ms=Math.max(ms,cs);
         }
         return ms;
+
+        //Bruteforce
         
+        // int ms=Integer.MIN_VALUE;
+        // int n=nums.length;
+        // for(int i=0;i<n;i++){
+        //     int cs=0;
+        //     for(int j=i;j<n;j++){
+        //         cs=cs+nums[j];
+        //         ms=Math.max(ms,cs);
+        //     }
+        // }
+        // return ms;
 
     }
 }
