d70e46662d410bd130d35894ee72accad756172f
Ashika Gupta <ashikagupta1503@gmail.com>
2025-09-01 13:02:32 +0000

    Sync LeetCode submission Runtime - 7 ms (34.19%), Memory - 45 MB (23.46%)

--- Leetcode_By_Ashika/0003-longest-substring-without-repeating-characters/solution.java
+++ Leetcode_By_Ashika/0003-longest-substring-without-repeating-characters/solution.java
@@ -2,25 +2,48 @@ import java.util.HashSet;
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int left = 0;
-        int maxLen = 0;
-        HashSet<Character> seen = new HashSet<>();
+        int n = s.length();
+        int left = 0, right = 0, maxLen = 0;
+        HashSet<Character> set = new HashSet<>();
 
-        for (int right = 0; right < s.length(); right++) {
-            // if char already in set, remove from left until unique
-            while (seen.contains(s.charAt(right))) {
-                seen.remove(s.charAt(left));
+        while (right < n) {
+            if (!set.contains(s.charAt(right))) {
+                set.add(s.charAt(right));
+                maxLen = Math.max(maxLen, right - left + 1);
+                right++;
+            } else {
+                set.remove(s.charAt(left));
                 left++;
             }
-
-            // add current char to set
-            seen.add(s.charAt(right));
-
-            // update max length
-            maxLen = Math.max(maxLen, right - left + 1);
         }
-
         return maxLen;
+
+
+
+    // int n=s.length();
+    // int maxLen=0;
+    // for(int i=0;i<n;i++){
+    //     for(int j=i;j<n;j++){
+    //         if (allUnique(s,i,j)){
+    //             maxLen = Math.max(maxLen,j - i + 1);
+    //         }
+            
+    //     }
+       
+    // } return maxLen;
+    // }
+    //  public static boolean allUnique(String s,int start,int end){
+    //     HashSet<Character> substr=new HashSet<>();
+    //     for(int i=start;i<=end;i++ ){
+              
+    //           if(substr.contains(s.charAt(i))){
+    //             return false;
+    //           }
+    //           substr.add(s.charAt(i));
+    //     }
+    //     return true;
+    
     }
+  
 }
 
