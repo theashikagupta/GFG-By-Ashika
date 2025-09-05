27757788c58fa22248583818a895804f43cf7397
Ashika Gupta <ashikagupta1503@gmail.com>
2025-09-05 05:51:08 +0000

    Sync LeetCode submission Runtime - 19 ms (95.46%), Memory - 55.4 MB (39.31%)

--- Leetcode_By_Ashika/0525-contiguous-array/solution.java
+++ Leetcode_By_Ashika/0525-contiguous-array/solution.java
@@ -1,26 +1,22 @@
-import java.util.HashMap;
-
 class Solution {
     public int findMaxLength(int[] nums) {
-        int n = nums.length;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        int currSum = 0;
-        map.put(0, -1);  // Handle the case when the subarray starts from index 0
-        
-        int result = 0;
-        
-        for (int i = 0; i < n; i++) {
-            currSum += (nums[i] == 1) ? 1 : -1;
-
-            if (map.containsKey(currSum)) {
-                result = Math.max(result, i - map.get(currSum));
-            } else {
-                map.put(currSum, i);
+        HashMap<Integer,Integer> map=new HashMap<>();
+        map.put(0,-1);
+        int sum=0; int maxLen=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                nums[i]=-1;
             }
         }
         
-        return result;
+        for(int i=0; i<nums.length ;i++){
+            sum+=nums[i];
+            if(map.containsKey(sum)){
+                maxLen=Math.max(maxLen,i-map.get(sum));
+            }else{
+                map.put(sum,i);
+            }
+            
+        }return maxLen;
     }
 }
-
