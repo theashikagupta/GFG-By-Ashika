29c33b9fa285c206c5811acf644d5f646c4c4c05
Ashika Gupta <ashikagupta1503@gmail.com>
2025-09-08 16:58:10 +0000

    Sync LeetCode submission Runtime - 858 ms (8.65%), Memory - 47.6 MB (82.11%)

---
+++ Leetcode_By_Ashika/0713-subarray-product-less-than-k/solution.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int n=nums.length;
+        int count=0; int prod=1;
+        
+        for(int i=0;i<n;i++){
+             for(int j=i;j<n;j++){
+                 prod*=nums[j];
+                 if(prod<k){ 
+                    count++; 
+                  }else {
+                   break;               
+                  } 
+            } 
+            prod=1;
+        } 
+        return count;
+
+        // for(int i=0;i<n;i++){              #my bruteforce approach       
+        //     for(int j=i;j<n;j++){
+        //         prod*=nums[j];
+        //         if(prod<k){
+        //             count++;
+                 
+        //         }else
+        //         {
+        //             break;
+        //         }
+                
+
+        //     }
+        //     prod=1;
+            
+            
+        // }
+       
+        // return count;
+
+
+
+    //Optimized approach -O(n)
+    //    if (k <= 1) return 0;
+    //     int prod = 1, count = 0;
+    //     int left = 0;
+        
+    //     for (int right = 0; right < nums.length; right++) {
+    //         prod *= nums[right];
+            
+    //         while (prod >= k) {
+    //             prod /= nums[left];
+    //             left++;
+    //         }
+            
+    //         count += (right - left + 1);
+    //     }
+        
+    //     return count;
+
+    }
+}
