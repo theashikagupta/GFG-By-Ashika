4aaf5b43330026646306704b6ddac543221ccdcc
Ashika Gupta <ashikagupta1503@gmail.com>
2025-09-09 02:55:28 +0000

    Sync LeetCode submission Runtime - 5 ms (75.20%), Memory - 44.6 MB (93.90%)

--- Leetcode_By_Ashika/0076-minimum-window-substring/solution.java
+++ Leetcode_By_Ashika/0076-minimum-window-substring/solution.java
@@ -12,11 +12,11 @@ class Solution {
         while (end < s.length()) {
             char ch = s.charAt(end);
             if (freq[ch] > 0) 
-            total--;  // char is needed
+            total--;  
             freq[ch]--;
-            end++;  // expand window
+            end++;  
 
-            // shrink window while valid
+           
             while (total == 0) {
                 if (end - start < minLen) {
                     minLen = end - start;
@@ -25,9 +25,8 @@ class Solution {
 
                 char startChar = s.charAt(start);
                 freq[startChar]++;
-                if (freq[startChar] > 0) total++;  // window lost a required char
-                start++;  // shrink
-            }
+                if (freq[startChar] > 0) total++; 
+                start++;              }
         }
 
         return (minLen == Integer.MAX_VALUE) ? "" : s.substring(minStart, minStart + minLen);
